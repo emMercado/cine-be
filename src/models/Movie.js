c9b9b5630047d5fd82a2cef2343e6957ce9a8796
emmamercado <mercadobemma55@gmail.com>
2022-11-15 11:36:39 -0300

    fix movies languages

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,16 +1,16 @@
-import mongoose, { model } from 'mongoose'
+import mongoose, { model } from "mongoose";
 
 const movieSchema = mongoose.Schema({
-    title: { type: String, required: false },
-    is3d: { type: String, required: false },
-    genres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],
-    direction: { type: String, required: false },
-    protagonists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Protagonist' }],
-    producer: { type: String, required: false },
-    date_premiere: { type: Date, required: false },
-    duration: { type: Number, required: false },
-    language: { type: mongoose.Schema.Types.ObjectId, ref: 'Language' },
-    img: { type: String, required: false }
-})
+  title: { type: String, required: false },
+  is3d: { type: String, required: false },
+  genres: [{ type: mongoose.Schema.Types.ObjectId, ref: "Genre" }],
+  direction: { type: String, required: false },
+  protagonists: [{ type: mongoose.Schema.Types.ObjectId, ref: "Protagonist" }],
+  producer: { type: String, required: false },
+  date_premiere: { type: Date, required: false },
+  duration: { type: Number, required: false },
+  languages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Language" }],
+  img: { type: String, required: false },
+});
 
 export default model("Movie", movieSchema);
